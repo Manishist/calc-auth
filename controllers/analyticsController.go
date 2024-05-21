a0b1a63883b4ada3029335a1b62f2e9b74346fb9
Manish <manish@HPLX-IT-LT-179.domain.name>
2024-05-21 19:37:05 +0530

    logs

--- controllers/analyticsController.go
+++ controllers/analyticsController.go
@@ -8,27 +8,20 @@ import (
 )
 
 func GetUserInfo(c *fiber.Ctx) error {
-	// Retrieve all users from the database
 	var users []models.User
 	if err := database.DB.Find(&users).Error; err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "Failed to fetch users",
 		})
 	}
-
-	// Prepare response data for each user
 	var responseData []fiber.Map
 	for _, user := range users {
-		// Calculate total time consumption
 		totalTimeConsumed := user.TotalTimeConsumed
 
-		// Calculate total time today
 		totalTimeToday := user.TotalTimeToday
 
-		// Calculate logged in days in the last 7 days
 		loggedInDaysLast7Days := user.LoggedInDaysLast7Days
 
-		// Prepare user data for response
 		userData := fiber.Map{
 			"name":                   user.Name,
 			"total_time_consumption": totalTimeConsumed,
